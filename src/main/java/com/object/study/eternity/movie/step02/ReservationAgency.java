4d265c6b94d0146593a8f09b9f566eebd7a9b024
kyuwon53 <lkw2v@naver.com>
2022-09-24 21:13:02 +0900

    Initial commit

---
+++ src/main/java/com/object/study/eternity/movie/step02/ReservationAgency.java
@@ -0,0 +1,10 @@
+package com.object.study.eternity.movie.step02;
+
+import com.object.study.eternity.money.Money;
+
+public class ReservationAgency {
+    public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
+        Money fee = screening.calculateFee(audienceCount);
+        return new Reservation(customer, screening, fee, audienceCount);
+    }
+}
---
+++ src/test/java/com/object/study/eternity/movie/step01/ReservationAgencyTest.java
@@ -0,0 +1,6 @@
+package com.object.study.eternity.movie.step01;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ReservationAgencyTest {
+
+}
